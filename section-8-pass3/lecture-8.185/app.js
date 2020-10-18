eff03de42411541cdd1761d5aa6c694e13f1e675
Robert Hieger <rhieger@gmail.com>
2020-10-18 11:53:18 -0400

    Create moreNumbers[] Array with Array() Constructor and new Keyword

---
+++ section-8-pass3/lecture-8.185/app.js
@@ -0,0 +1,17 @@
+// Method #1: Using Square Bracket Notation []
+
+const numbers = [22, 46, -72, 36];
+
+console.log('Create numbers[] Array Using Bracket Notation...\n');
+console.log('numbers[] =>\n');
+console.table(numbers);
+
+// Method #2: Using Array() Constructor with new Keyword
+
+const moreNumbers = new Array(5, 37);
+
+console.log(
+  'Create moreNumbers[] Array Using Array() Constructor and new Keyword...\n'
+);
+console.log('moreNumbers[] =>\n');
+console.table(moreNumbers);
