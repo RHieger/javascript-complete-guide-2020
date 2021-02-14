a769870b519929e17db5401390b24ef7bc22a39f
Robert Hieger <rhieger@gmail.com>
2021-02-13 19:45:19 -0500

    Migrate code from lecture-9.215 to lecture-9.216

---
+++ section-9/lecture-9.216/assets/scripts/objects.js
@@ -0,0 +1,13 @@
+const person = {
+  name: 'Bobby',
+  age: 58,
+  hobbies: [
+    'Computer gaming',
+    'Reading'
+  ],
+  greet: function() {
+    alert('Hi there!');
+  }
+};
+
+person.greet();
