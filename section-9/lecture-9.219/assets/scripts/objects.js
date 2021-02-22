787fb88daf913a4e0ee74f26a384a5e8d7b7e717
Robert Hieger <rhieger@gmail.com>
2021-02-22 09:00:18 -0500

    Complete Lecture 9.219: Add property to object dynamically

--- section-9/lecture-9.219/assets/scripts/objects.js
+++ section-9/lecture-9.219/assets/scripts/objects.js
@@ -18,6 +18,26 @@ const person = {
   }
 };
 
+// DEMO: Adding user-chosen constant name. Here
+// the value is hard-coded only for demonstration
+// purposes, but the value could just as easily
+// come from user input in a form, for example.
+
+const userChosenKeyName = 'level';
+
+const person2 = {
+  'first name': 'Humpy',
+  age: 12,
+  hobbies: [
+    'Skulking',
+    'Sniveling'
+  ],
+  greet: function() {
+    alert('Hi there!');
+  },
+  [userChosenKeyName]: userChosenKeyName
+};
+
 // Use square bracket notation to log 'first name'
 // key to console.
 
@@ -36,5 +56,8 @@ const person = {
 
 const keyName = 'first name';
 
-console.log("Value of person[keyName] =>\n");
-console.log(person[keyName]);
+console.log('Value of person[keyName] =>\n');
+console.log(person[keyName], '\n');
+
+console.log('Value of person2[userChosenKeyName] =>\n')
+console.log(person2[userChosenKeyName], '\n');
