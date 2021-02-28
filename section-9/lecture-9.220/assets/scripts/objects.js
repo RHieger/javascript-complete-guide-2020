4a43478c7187cb30893959ef30b60c073c834216
Robert Hieger <rhieger@gmail.com>
2021-02-28 16:32:16 -0500

    Add data validation to addMovieHandler()

--- section-9/lecture-9.220/assets/scripts/objects.js
+++ section-9/lecture-9.220/assets/scripts/objects.js
@@ -2,7 +2,17 @@ const addMovieButton = document.getElementById('add-movie-btn');
 const searchButton = document.getElementById('search-btn');
 
 const addMovieHandler = () => {
+  
   const title = document.getElementById('title').value;
   const extraName = document.getElementById('extra-name').value;
   const extraValue = document.getElementById('extra-value').value;
+
+  if (
+      title.trim() === '' ||
+      extraName.trim() === '' ||
+      extraValue.trim() === '' ||
+    ) {
+      return;
+  }
+
 };
