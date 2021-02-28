14e097ac11a43131d32634c085e0830eb6b374ee
Robert Hieger <rhieger@gmail.com>
2021-02-28 16:26:12 -0500

    Capture movie data nodes from DOM in addMovieHandler()

--- section-9/lecture-9.220/assets/scripts/objects.js
+++ section-9/lecture-9.220/assets/scripts/objects.js
@@ -1,4 +1,8 @@
 const addMovieButton = document.getElementById('add-movie-btn');
 const searchButton = document.getElementById('search-btn');
 
-const addMovieHandler = () => {};
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+};
