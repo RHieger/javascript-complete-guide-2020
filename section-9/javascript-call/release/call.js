c760412988775b160952c223060b8dcee1f4a9c6
Robert Hieger <rhieger@gmail.com>
2021-04-04 16:20:45 -0400

    Create stub function stubShow() and tested whether it evaluates as function

---
+++ section-9/javascript-call/release/call.js
@@ -0,0 +1,10 @@
+function stubShow() {
+  // function stub for demo only
+}
+
+console.log('\nFunction stubShow() created.\n');
+console.log(
+  'Is stubShow() a function?',
+  stubShow instanceof Function,
+  '\n\n'  
+);
