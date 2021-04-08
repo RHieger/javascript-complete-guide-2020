77e13e35a463ccc49944b505e5a4eed553187401
Robert Hieger <rhieger@gmail.com>
2021-04-08 01:12:52 -0400

    Initialize app.js and capture all required elements

---
+++ section-9/javascript-call/js/app.js
@@ -0,0 +1,16 @@
+// Capture necessary elements
+const breedOne = document.getElementById('favBreed1');
+const breedOrigin1 = document.getElementById('origin1');
+const breedAvgLife1 = document.getElementById('avgLife1');
+
+const breedTwo = document.getElementById('favBreed2');
+const breedOrigin2 = document.getElementById('origin2');
+const breedAvgLife2 = document.getElementById('avgLife2');
+
+const breedThree = document.getElementById('favBreed3');
+const breedOrigin3 = document.getElementById('origin3');
+const breedAvgLife3 = document.getElementById('avgLife3');
+
+const button1 = document.getElementById('breed1');
+const button2 = document.getElementById('breed2');
+const button3 = document.getElementById('breed3');
